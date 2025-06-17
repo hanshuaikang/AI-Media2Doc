5960d180b9ecb4a066394f7028035337a8182436
hanshuaikang <1758504262@qq.com>
2025-06-17 23:42:57 +0800

    feat: llm 大模型调用改为使用 openai sdk 进行

--- backend/actions/llm.py
+++ backend/actions/llm.py
@@ -1,42 +1,46 @@
 # -*- coding: UTF-8 -*-
-from arkitect.core.component.context.context import Context
 from arkitect.core.component.llm import ArkChatRequest
-from arkitect.types.llm.model import ArkChatParameters
+from openai import OpenAI
 
-from env import MODEL_ID
+import env
 from .dispatcher import ActionDispatcher
 
 
 @ActionDispatcher.register("generate_markdown_text")
 async def generate_markdown_text(request: ArkChatRequest):
-    parameters = ArkChatParameters(**request.__dict__)
-    ctx = Context(model=MODEL_ID, parameters=parameters)
-    await ctx.init()
+    client = OpenAI(
+        # 此为默认路径，您可根据业务所在地域进行配置
+        base_url=env.BASE_URL,
+        # 从环境变量中获取您的 API Key。此为默认方式，您可根据需要进行修改
+        api_key=env.LLM_API_KEY,
+    )
     messages = [
         {"role": message.role, "content": message.content}
         for message in request.messages
     ]
-    resp = await ctx.completions.create(messages=messages, stream=request.stream)
 
-    if request.stream:
-        async for chunk in resp:
-            yield chunk
-    else:
-        yield resp
+    yield client.chat.completions.create(
+        # 指定您创建的方舟推理接入点 ID，此处已帮您修改为您的推理接入点 ID
+        model=env.MODEL_ID,
+        messages=messages,
+    )
 
 
 @ActionDispatcher.register("default")
 async def default_llm_action(request: ArkChatRequest):
-    parameters = ArkChatParameters(**request.__dict__)
-    ctx = Context(model=MODEL_ID, parameters=parameters)
-    await ctx.init()
+    client = OpenAI(
+        # 此为默认路径，您可根据业务所在地域进行配置
+        base_url=env.BASE_URL,
+        # 从环境变量中获取您的 API Key。此为默认方式，您可根据需要进行修改
+        api_key=env.LLM_API_KEY,
+    )
     messages = [
         {"role": message.role, "content": message.content}
         for message in request.messages
     ]
-    resp = await ctx.completions.create(messages=messages, stream=request.stream)
-    if request.stream:
-        async for chunk in resp:
-            yield chunk
-    else:
-        yield resp
+
+    yield client.chat.completions.create(
+        # 指定您创建的方舟推理接入点 ID，此处已帮您修改为您的推理接入点 ID
+        model=env.MODEL_ID,
+        messages=messages,
+    )
