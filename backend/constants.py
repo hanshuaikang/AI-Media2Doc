f5ffdd93958ab3995e9ad3f8630e6f38929dfaf5
hanshuaikang <1758504262@qq.com>
2025-08-30 00:18:06 +0800

    feat: 重构新的 fastapi 实现

---
+++ backend/constants.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class VolcengineASRResponseStatusCode(enum.Enum):
+    SUCCESS = 1000
+    RUNNING = 2000
+    PENDING = 2001
+
+
+class AsrTaskStatus(enum.Enum):
+    RUNNING = "running"
+    FINISHED = "finished"
+    FAILED = "failed"
